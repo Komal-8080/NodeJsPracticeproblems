76b8bb6cb542ba32c8d29b972bd38d3de00fd163
Komal-8080 <shindeg163@gmail.com>
2021-02-13 01:49:50 +0530

    ADDED: Store Result in a Dictionary

--- MapPracticeProblems/dieRollMap.js
+++ MapPracticeProblems/dieRollMap.js
@@ -1,6 +1,12 @@
 let valid = true;
+let diceResult = new Map([ [1,0],[2,0],[3,0],[4,0],[5,0],[6,0] ]);
 
 while (true){
     let dice = Math.floor(Math.random() * 6)+1;
-    console.log("Dice Value is: "+dice);
+    let rolls = diceResult.get(dice);
+    for (let [key, val] of diceResult){
+        console.log("Dice value is: "+key+ " for "+val+ "times");
+        rolls++;
+        diceResult.set(dice, rolls);
+    }
 }
