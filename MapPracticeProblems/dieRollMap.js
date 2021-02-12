5c4c496b7cec273fdb20a008e3544c1dff7541b3
Komal-8080 <shindeg163@gmail.com>
2021-02-13 01:33:29 +0530

    ADDED: Dice Rolled Repeadtedly and Result found

--- MapPracticeProblems/dieRollMap.js
+++ MapPracticeProblems/dieRollMap.js
@@ -1,2 +1,6 @@
-let dice = Math.floor(Math.random() * 6)+1;
-console.log("Dice Value is: "+dice);
+let valid = true;
+
+while (true){
+    let dice = Math.floor(Math.random() * 6)+1;
+    console.log("Dice Value is: "+dice);
+}
