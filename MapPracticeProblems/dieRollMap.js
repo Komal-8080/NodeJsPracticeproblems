292e3f1519c537684f7ed1f5761b35a9c381d823
Komal-8080 <shindeg163@gmail.com>
2021-02-14 00:32:28 +0530

    ADDED: To Repeat Till any One REaches 10

--- MapPracticeProblems/dieRollMap.js
+++ MapPracticeProblems/dieRollMap.js
@@ -1,12 +1,22 @@
 let valid = true;
 let diceResult = new Map([ [1,0],[2,0],[3,0],[4,0],[5,0],[6,0] ]);
+let count = false;
 
 while (true){
     let dice = Math.floor(Math.random() * 6)+1;
     let rolls = diceResult.get(dice);
     for (let [key, val] of diceResult){
-        console.log("Dice value is: "+key+ " for "+val+ "times");
-        rolls++;
-        diceResult.set(dice, rolls);
+        if (val == 10){            
+            count = true;
+            break;
+        }
+    } 
+    if (count == true)
+    break; 
+    rolls++;
+    diceResult.set(dice, rolls);
+}           
+    for (let [key, val] of diceResult){
+        console.log("Dice value is: "+key+ " for "+val+ "times"); 
     }
-}
+
