aec55bab1f380f46ac61065f48260d937393fcb4
Komal-8080 <shindeg163@gmail.com>
2021-02-14 01:01:01 +0530

    ADDED: To Get Number That Reached Maximum And Minimum Times

--- MapPracticeProblems/dieRollMap.js
+++ MapPracticeProblems/dieRollMap.js
@@ -20,3 +20,15 @@ while (true){
         console.log("Dice value is: "+key+ " for "+val+ "times"); 
     }
 
+let diceValuesArray = Array.from(diceResult.values()).sort(function (a, b) { return a - b });
+let max = diceValuesArray[diceValuesArray.length - 1];
+let min = diceValuesArray[0];
+function getKey(map, value) {
+    for (let [key, val] of diceResult) {
+        if (val == value) {
+                return key;
+        }
+    }
+}
+console.log(getKey(diceResult, max)+" has reached maximum times i.e.,"+max+ " times");
+console.log(getKey(diceResult, min)+" has reached minimum times i.e.,"+min+ " times");
